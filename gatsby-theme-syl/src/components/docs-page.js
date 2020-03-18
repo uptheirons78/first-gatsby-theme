6114d34d593d96b458ede63f4bda99936eb0680d
mauro bono <bono.mauro@gmail.com>
2020-03-18 15:59:06 +0100

    feat(gatsby-theme-syl) Add template file for DocsPage

---
+++ gatsby-theme-syl/src/components/docs-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from './layout';
+
+const DocsPage = ({ page }) => {
+  return (
+    <Layout>
+      <h1>{page.title}</h1>
+      <MDXRenderer>{page.body}</MDXRenderer>
+      <p>This page was updated {page.updated}.</p>
+    </Layout>
+  );
+};
+
+export default DocsPage;
